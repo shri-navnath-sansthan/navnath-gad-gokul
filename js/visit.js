bff61fdb569dec0b9d67eeafd7a2d4b9732403aa
shri-navnath-sansthan <navnathmaharajgokul@gmail.com>
2026-03-10 17:45:16 +0530

    update css and remove visit.js

--- js/visit.js
+++
@@ -1,7 +0,0 @@
-fetch("https://api.countapi.xyz/hit/navnathgad_counter/visits")
-.then(function(response){
-return response.json();
-})
-.then(function(data){
-document.getElementById("visitor-count").innerText = data.value;
-});
